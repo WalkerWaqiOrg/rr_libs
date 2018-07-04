afd2b4987641971c5f002ef7b577245667568e44
Mike2132976 <>
2018-07-04 11:52:33 +0800

    1.Add function implementation 2.Add extern "C" 3.Add sub-libs 4.Set output directory for all libs

--- include/rr_libs.h
+++ include/rr_libs.h
@@ -1,6 +1,14 @@
 #ifndef RR_LIBS_LIBRARY_H
 #define RR_LIBS_LIBRARY_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+void rr_slow_hash(const void *data, unsigned int length, unsigned char *hash);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // RR_LIBS_LIBRARY_H
--- rr_libs.cpp
+++ rr_libs.cpp
@@ -1,1 +1,6 @@
 #include "rr_libs.h"
+#include "hash.h"
+
+void rr_slow_hash(const void *data, unsigned int length, unsigned char *hash) {
+    crypto::cn_slow_hash(data, length, reinterpret_cast<crypto::hash&>(hash));
+}
