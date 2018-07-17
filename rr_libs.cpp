ac35a95be68cb626a747329ce20087cdf73b5ccf
Mike2132976 <>
2018-07-17 13:30:35 +0800

    fix

--- rr_libs.cpp
+++ rr_libs.cpp
@@ -2,5 +2,5 @@
 #include "hash.h"
 
 void rr_slow_hash(const void *data, unsigned int length, unsigned char *hash) {
-    crypto::cn_slow_hash(data, length, reinterpret_cast<crypto::hash&>(hash));
+    crypto::cn_slow_hash(data, length, (char *)hash);
 }
